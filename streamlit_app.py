bf1b706de35a3bf5ae2225aa11fba8df7b149435
RR <rajeshravindran05@gmail.com>
2023-02-03 21:21:17 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('cereals')
+streamlit.text('Sandwich')
+streamlit.text('Egg')
 
